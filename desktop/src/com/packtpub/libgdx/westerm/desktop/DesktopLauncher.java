3e651c4f336a14facaab89a3fef7ee89bcc8c4d5
Elmar Kengerly <MilitaryCat48@gmail.com>
2018-07-11 22:30:10 +0300

    First initialization

---
+++ desktop/src/com/packtpub/libgdx/westerm/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packtpub.libgdx.westerm.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.libgdx.westerm.Westerm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Westerm(), config);
+	}
+}
